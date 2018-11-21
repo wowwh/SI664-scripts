7181b8184dd2cbdb6f7bcb80a851f17a81a0b547
Hao Wu <wowwh@umich.edu>
2018-11-21 16:17:59 -0500

    hw10

---
+++ scripts/mysite/secrets.py
@@ -0,0 +1,4 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ov_sbg)(^$8&eqe8&urmu(^#(slcloca=mt0^gol6=n+jbm@va'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '151349406761-dpqoglajnvc3njl4pnc38fm7oif6m8kf.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'VVgvnVziIn5YxTvVsOqVhdx4'
